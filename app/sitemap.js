96b6992143c1b9fbfc9faf26bae6807d4eab01a0
Hassan <soonh127@gmail.com>
2026-07-19 14:24:01 +0300

    add robots.txt and sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    { url: 'https://www.nukhbahr.com', lastModified: new Date() },
+    { url: 'https://www.nukhbahr.com/for-companies', lastModified: new Date() },
+    { url: 'https://www.nukhbahr.com/auth/login', lastModified: new Date() },
+    { url: 'https://www.nukhbahr.com/privacy', lastModified: new Date() },
+    { url: 'https://www.nukhbahr.com/terms', lastModified: new Date() },
+    { url: 'https://www.nukhbahr.com/contact', lastModified: new Date() },
+  ]
+}
